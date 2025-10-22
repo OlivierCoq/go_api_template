c586929d18572f7c79fb7245dfa141122786c6c0
Olivier <coq.olivier36@gmail.com>
2025-10-22 09:05:04 -0400

    basic middleware auth check

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -3,8 +3,10 @@ package middleware
 import (
 	"context"
 	"net/http"
+	"strings"
 
 	"github.com/OlivierCoq/go_api_template/internal/store"
+	"github.com/OlivierCoq/go_api_template/internal/utils"
 )
 
 type UserMiddleware struct {
@@ -37,4 +39,25 @@ func GetUser(r *http.Request) *store.User {
 		panic("missing user in request") // bad actor call. This could be a hacker trying to access a protected route without authentication.
 	}
 	return user
+}
+
+func (um *UserMiddleware) Middleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		w.Header().Add("Vary", "Authorization") // Caching proxies should consider the Authorization header when deciding whether to serve a cached response.
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "" {
+			// No auth header, so we set the user as anonymous and proceed to the next handler:
+			r = SetUser(r, store.AnonymousUser)
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		headerParts := strings.Split(authHeader, " ") // Bearer tokenstring
+		if len(headerParts) != 2 || headerParts[0] != "Bearer" {
+			// Invalid auth header format, so we set the user as anonymous and proceed to the next handler:
+			utils.WriteJSON(w, http.StatusUnauthorized, utils.Envelope{"error": "invalid authorization header format"})
+			return
+		}
+	})
 }
